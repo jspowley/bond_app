9659110900fa851a0862262584c844f9c9455cd8
Travis <tnowak@ualberta.ca>
2025-03-03 20:50:25 -0700

    Visuals progress

--- ui.R
+++ ui.R
@@ -16,63 +16,6 @@ library(shinydashboard)
 ui <- bslib::page_navbar(
     title = "Portfolio Management Dashboard",
     theme = bs_theme(bootswatch = "flatly"),
-<<<<<<< HEAD
-    
-    
-    nav_panel(
-      title = "About",
-      fluidPage(
-        h2("Welcome"),
-        p("Insert content here for tab 1.")
-      )
-    ),
-      
-    nav_panel(title = "Sensitivity Analysis",
-              bslib::layout_sidebar(
-                sidebar = sidebar(
-                  
-                             card(card_header("Model"),
-                                  uiOutput("training_window_dates"),
-                                  shiny::numericInput("delta_lag", "Risk Time Frame (Trading Days)", min = 1, max = 3650, value = 1)),
-                             
-                             card(card_header("Yield"),
-                                  dateInput("selected_yield", "Select Yield Curve Date:", min = as.Date(ui_date_min), max = as.Date(ui_date_max), value = ui_date_max),
-                                  actionButton("max_date", "Reset to Most Recent")),
-                             
-                             card(card_header("Scenario, (Z Score)"),
-                                  sliderInput("parallel_shift", "Level Factor",
-                                              min = -4, max = 4, value = 0, step = 0.05),
-                                  sliderInput("steepening", "Steepening Factor",
-                                              min = -4, max = 4, value = 0, step = 0.05),
-                                  sliderInput("curvature", "Curvature Factor",
-                                              min = -4, max = 4, value = 0, step = 0.05)),
-                             textOutput("mono"),
-                             width = 800,
-                             
-                             actionButton("addBond", "Add Bond"),
-                             actionButton("subBond", "Remove Bond"),
-                             uiOutput("bond_inputs")
-                             # Reactive Counter To Increase/Decrease Bonds
-                             #https://www.youtube.com/watch?v=ML54auObmL8
-                    ),
-                # https://www.youtube.com/watch?v=O6WLERr5bKU&t=470s
-                bslib::layout_columns(
-                  col_widths = c(12,4,4,4,12),
-                  
-                  bslib::card(
-                    plotOutput("yield_curve_plot")),
-                  uiOutput("current_curve_scalar_value"),
-                  uiOutput("stressed_curve_scalar_value"),
-                  uiOutput("pct_chg"),
-                  
-                  bslib::card(
-                    plotOutput("multi_plot"),
-                    full_screen = TRUE
-                  )
-                  )
-                )
-              )
-=======
 
     nav_panel(
         title = "Tab 1",
@@ -135,11 +78,9 @@ ui <- bslib::page_navbar(
         
         col_widths = c(12,4,4,4,12)
         )
-            
->>>>>>> edba65ab8171f21caf7c351fdad30a18a4dae65a
+        )
     )
     )
-)
 
     
     
